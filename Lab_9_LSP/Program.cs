262130558959ac23e5de441238c5e22ebc5d5218
Mohamed Khalil <mkhalil2@ksu.edu>
2020-10-12 18:09:47 -0500

    Add project files.

---
+++ Lab_9_LSP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab_9_LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+
+            Square s1 = new Square();
+
+            Console.WriteLine("Now, calling the function with a Rectanlge");
+            UserOfRectangle(r1);
+
+            Console.WriteLine("Now, calling the function with a Square");
+            UserOfRectangle(s1);
+            Console.ReadLine();
+        }
+
+        static void UserOfRectangle(Rectangle r)
+        {
+            r.Height = 5;
+            r.Width = 4;
+            if (r.CalculateArea() != 20)
+            {
+                throw new Exception("Bad Area!");
+            }
+            else
+            {
+                Console.Write("For the Shape with sides: " + r.Height + " by " + r.Width + ", ");
+                Console.WriteLine("The area is: " + r.CalculateArea());
+            }
+        }
+    }
+}
---
+++ Lab_9_LSP/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_9_LSP
+{
+    public class Rectangle
+    {
+        public virtual int Height { get; set; }
+        public virtual int Width { get; set; }
+
+        public int CalculateArea()
+        {
+            return Height * Width;
+        }
+
+
+    }
+
+    class Square : Rectangle
+    {
+        public override int Height
+        {
+            get { return base.Height; }
+            set { base.Height = base.Width = value; }
+        }
+
+        public override int Width
+        {
+            get { return base.Height; }
+            set { base.Height = base.Width = value; }
+        }
+    }
+}
